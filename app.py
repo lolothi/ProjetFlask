ba1177b22d1c8aea9c5ec1ec134b02c5588703ea
Vincent <vincent@debian>
2023-03-27 16:13:29 +0200

    updateSQL

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+import sqlite3
+from flask import g,Flask
 
 app = Flask(__name__)
+PATH_DATABASE = 'imcpersonnes.db'
+PATH = "/home/vincent/myproject/ProjetFlask/"
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def get_db():
+    return sqlite3.connect(PATH_DATABASE)	
+
+confSQL = open(PATH+"confSQL.sql","r")
+
+db = get_db()
+#print(confSQL.read())
+db.executescript(confSQL.read())
+
+
+
+
