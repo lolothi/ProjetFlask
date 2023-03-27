84f36928fb1125b5cb556c26a9daba2d57edbe3f
FAUCHERYBenoit <118353740+FAUCHERYBenoit@users.noreply.github.com>
2023-03-27 16:13:55 +0200

    feature/user-profil
    * Template, TODO: connect to DB
    
    * clean code
    
    ---------
    
    Co-authored-by: benoit.fauchery <Benoit.Fauchery@cgi.com>

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, redirect, session, request, jsonify
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 html = "index.html"
-
 
 # Page d'accueil
 @app.route("/")
 def main_page():
     return render_template('index.html')
-
 
 # Login
 @app.route("/login")
@@ -17,9 +16,42 @@ def login():
     return render_template('index.html')
 
 # User profile
-@app.route("/user")
+@app.route("/user", methods=['GET', 'POST'])
 def profil():
-    return render_template('index.html')
+    error = None
+
+    #TODO : Get from DB
+    username = ""
+    password = ""
+    email = ""
+    age = None
+
+    if request.method == 'POST':
+
+        dom_username = request.form.get('username')
+        dom_password = request.form.get('password')
+        dom_email = request.form.get('email')
+        dom_age = request.form.get('age')
+
+        if(valid_profil(dom_username,dom_password,dom_email,dom_age)):
+            session['user'] = dom_email
+            #TODO : Update user row in db
+            return redirect("/imc")
+        else:
+            error = "One of the field is null"
+    
+    return render_template(
+        'user-profil.html', 
+        username = username, 
+        email = password, 
+        password = email,
+        age = age,
+        error = error)
+
+def valid_profil(username:str,email:str,password:str,age:str):
+    if(len(username) == 0 or len(email) == 0 or len(password) == 0 or len(age) == 0):
+        return False
+    return True
 
 @app.route("/imc", methods=['POST', 'GET'])
 def imc():
@@ -43,7 +75,6 @@ def imc():
         return render_template('imc.html', imc=imc, imc_color=imc_color) # passe le résultat de l'IMC à votre modèle HTML
     
     return render_template('imc.html')
-
 
 def computeImc(poids, taille):
     return round(poids / ((taille / 100) ** 2) , 2)
