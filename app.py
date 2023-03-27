1fe0ef1b7e4b377d5fe816d932630df1f88bd5ba
FAUCHERYBenoit <118353740+FAUCHERYBenoit@users.noreply.github.com>
2023-03-27 10:43:24 +0200

    feature/template_connecting
    Co-authored-by: benoit.fauchery <Benoit.Fauchery@cgi.com>

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+html = "index.html"
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def main_page():
+    return render_template('index.html')
