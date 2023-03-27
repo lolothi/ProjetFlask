49bf704db245e430e94a0b51452cdba7f9885fcf
FAUCHERYBenoit <118353740+FAUCHERYBenoit@users.noreply.github.com>
2023-03-27 11:07:30 +0200

    feature/repartition-ecran
    Co-authored-by: benoit.fauchery <Benoit.Fauchery@cgi.com>

--- app.py
+++ app.py
@@ -4,6 +4,25 @@ app = Flask(__name__)
 
 html = "index.html"
 
+
+# Page d'accueil
 @app.route("/")
 def main_page():
+    return render_template('index.html')
+
+
+# Login
+@app.route("/login")
+def login():
+    return render_template('index.html')
+
+# IMC
+@app.route("/imc")
+def imc():
+    return render_template('index.html')
+
+
+# User profile
+@app.route("/user")
+def profil():
     return render_template('index.html')
