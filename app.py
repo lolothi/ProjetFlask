1f07ab0a6f75637dc93c0fe0496cf9763a032628
Victor BARDIN <victor.bardin@ipilyon.net>
2023-03-27 16:36:40 +0200

    updateSQL (#6)
    Co-authored-by: Vincent <vincent@debian>
    Co-authored-by: Vin100fun <115066719+Vin100fun@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
+
 from flask import Flask, render_template, redirect, session, request, jsonify
+import sqlite3
+
+app = Flask(__name__)
+PATH_DATABASE = 'imcpersonnes.db'
+PATH = "/home/vincent/myproject/ProjetFlask/"
+
+def get_db():
+    return sqlite3.connect(PATH_DATABASE)	
+
+confSQL = open(PATH+"confSQL.sql","r")
+
+db = get_db()
+#print(confSQL.read())
+db.executescript(confSQL.read())
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
