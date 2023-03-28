040cd247dd3f7fd9dc296cba460739e11ce34677
Vincent <vincent@debian>
2023-03-28 23:29:30 +0200

    fonctionsSQL

--- confSQL.sql
+++ confSQL.sql
@@ -6,16 +6,17 @@ lastName VARCHAR(30),
 firstName VARCHAR(30), 
 mail VARCHAR(30)NOT NULL UNIQUE, 
 passwd VARCHAR(30)NOT NULL, 
-age integer
+age smallint
 );
 
 
-CREATE TABLE IF NOT EXISTS History (id integer primary key autoincrement, 
-height double, 
-weight double, 
-idUsers integer, 
+CREATE TABLE IF NOT EXISTS History (
+id integer primary key autoincrement, 
+height smallint, 
+weight decimal(4,2), 
+idUser integer, 
 date_create date,
-FOREIGN KEY (idUsers) REFERENCES Users(id)
+FOREIGN KEY (idUser) REFERENCES Users(id)
 );
 
 
