760dd98c43def50316ca55159dff2ac03234ac97
Laurent <101107400+lolothi@users.noreply.github.com>
2023-03-29 19:29:42 +0200

    Feature : login/register/logout (#12)
    * login logout in progress
    
    * waiting db
    
    * add steesheet for base.html
    
    * update after merge - delete duplication
    
    * comment and refactoring login,register,logout part
    
    * update declaration of variables

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
-from flask import Flask, render_template, redirect, session, request, jsonify
+from flask import Flask, render_template, request, session, redirect
 import sqlite3
 
 app = Flask(__name__)
-NAME_DATABASE = 'imcpersonnes.db'
-PATH = "./"
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-#Modify PATH to your environment
+NAME_DATABASE = 'imcpersonnes.db'
 
 def get_db():
     return sqlite3.connect(NAME_DATABASE)	
-
-
 #print(confSQL.read())
 
+#Connect to DB
+db = get_db()
+
+#Get parameters for DB
+confSQL = open("confSQL.sql","r")
+
+#Create tables if needed
+db.executescript(confSQL.read())
+
+#Tests
+#db.execute("insert into Users (lastName,firstName,mail,passwd,age) values ('EVIEUX','Vincent','vincent@mail.com','motdepasse',25)")
+#db.execute("insert into History (height,weight,idUser,date_create) values (177,70.5,1,'2022-03-28')")
+#print(isAccountOK("vincent@mail.com","motdepasse"))
+#print(getWeightUser("vincent@mail.com"))
+#print(getHeightUser("vincent@mail.com"))
+#print(getUserInfo("vincent@mail.com"))
 
 def isAccountOK(mail,passwd) :
 	reqSQL = "select passwd from Users where mail = "
@@ -69,71 +82,107 @@ def getUserInfo(user) :
 		return res
 	return False
 		
-
-
-
-app = Flask(__name__)
-
-
-app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-
-html = "index.html"
+# html = "index.html"
 
 # Page d'accueil
 @app.route("/")
 def main_page():
-    return render_template('index.html')
+    return render_template("index.html")
 
 # Login
-@app.route("/login")
+user_db = []  # Liste de dict : en attendant l'acces à la db
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    return render_template('index.html')
+    error = None 
+    message = None
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        # check if user is connected
+        connected_user = session["user"]
+    except:
+        session["user"] = None
+        connected_user = None
+
+    if request.method == "POST":
+        # vérifier l'existence de l'email dans la db (list of dict)
+        # if (any(email in d['email'] for d in user_db)):
+        for user in user_db:
+            if email == user["email"]:
+                if password == user["password"]:
+                    session["user"] = email
+                    connected_user = email
+                    message = "utilisateur connecté"
+                    break
+                else:
+                    message = None
+                    error = "mauvais mot de passe"
+                    break
+        else:
+            error = "mauvais utilisateur/mot de passe"
+
+    return render_template("login.html", message=message, error=error, connected_user=connected_user)
+
+# Register user if new one
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    message = None
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if request.method == "POST":
+        if email and password:
+            user_db.append({"email": email, "password": password})
+            message = "utilisateur créé"
+
+    return render_template("register.html", message=message)
+
+# Logout user if connected
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect('/')
 
 # User profile
-@app.route("/user", methods=['GET', 'POST'])
+@app.route("/user", methods=["GET", "POST"])
 def profil():
     error = None
 
-    #TODO : Get from DB
+    # TODO : Get from DB
     username = ""
     password = ""
     email = ""
     age = None
 
-    if request.method == 'POST':
+    if request.method == "POST":
 
-        dom_username = request.form.get('username')
-        dom_password = request.form.get('password')
-        dom_email = request.form.get('email')
-        dom_age = request.form.get('age')
+        dom_username = request.form.get("username")
+        dom_password = request.form.get("password")
+        dom_email = request.form.get("email")
+        dom_age = request.form.get("age")
 
-        if(valid_profil(dom_username,dom_password,dom_email,dom_age)):
-            session['user'] = dom_email
-            #TODO : Update user row in db
+        if valid_profil(dom_username, dom_password, dom_email, dom_age):
+            session["user"] = dom_email
+            # TODO : Update user row in db
             return redirect("/imc")
         else:
             error = "One of the fields is null"
-    
-    return render_template(
-        'user-profil.html', 
-        username = username, 
-        email = password, 
-        password = email,
-        age = age,
-        error = error)
 
-def valid_profil(username:str,email:str,password:str,age:str):
-    if(len(username) == 0 or len(email) == 0 or len(password) == 0 or len(age) == 0):
+    return render_template("user-profil.html", username=username, email=password, password=email, age=age, error=error)
+
+
+def valid_profil(username: str, email: str, password: str, age: str):
+    if len(username) == 0 or len(email) == 0 or len(password) == 0 or len(age) == 0:
         return False
     return True
 
 
-@app.route("/imc", methods=['POST', 'GET'])
+@app.route("/imc", methods=["POST", "GET"])
 def imc():
-    if request.method == 'POST':
-        poids = float(request.form['poids'])
-        taille = float(request.form['taille'])
+    if request.method == "POST":
+        poids = float(request.form["poids"])
+        taille = float(request.form["taille"])
         imc = computeImc(poids, taille)
         if imc:
             if imc < 16:
@@ -142,35 +191,20 @@ def imc():
                 imc_color = "jaune"
             elif imc < 24:
                 imc_color = "vert"
-            elif imc <26:
-                imc_color="jaune"
+            elif imc < 26:
+                imc_color = "jaune"
             else:
                 imc_color = "rouge"
-    
-        #redirect vers /imc
-        return render_template('imc.html', imc=imc, imc_color=imc_color) # passe le résultat de l'IMC à votre modèle HTML
-    
-    return render_template('imc.html')
-    
+
+        # redirect vers /imc
+        return render_template(
+            "imc.html", imc=imc, imc_color=imc_color
+        )  # passe le résultat de l'IMC à votre modèle HTML
+
+    return render_template("imc.html")
+
+
 def computeImc(poids, taille):
-    return round(poids / ((taille / 100) ** 2) , 2)
+    return round(poids / ((taille / 100) ** 2), 2)
     
-    
-    
-    
-#Get parameters for DB
-confSQL = open(PATH+"confSQL.sql","r")
 
-#Connect to DB
-db = get_db()
-
-#Create tables if needed
-db.executescript(confSQL.read())
-
-#Tests
-#db.execute("insert into Users (lastName,firstName,mail,passwd,age) values ('EVIEUX','Vincent','vincent@mail.com','motdepasse',25)")
-#db.execute("insert into History (height,weight,idUser,date_create) values (177,70.5,1,'2022-03-28')")
-#print(isAccountOK("vincent@mail.com","motdepasse"))
-#print(getWeightUser("vincent@mail.com"))
-#print(getHeightUser("vincent@mail.com"))
-#print(getUserInfo("vincent@mail.com"))
